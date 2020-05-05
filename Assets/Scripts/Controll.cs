47f15efc5a3cb23942032bcea8e607c8855e25f4
Kirill <Kubirill@rambler.ru>
2020-05-05 10:35:27 +0300

    Start project

---
+++ Assets/Scripts/Controll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Controll 
+{
+    public static float GetHorizontalMove()
+    {
+        return Input.GetAxis("Horizontal");
+    }
+    public static bool GetJumpStart()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
+    }
+    public static bool GetJumpHold()
+    {
+        return Input.GetKey(KeyCode.Space);
+    }
+    public static bool GetJumpStop()
+    {
+        return Input.GetKeyUp(KeyCode.Space);
+    }
+}
